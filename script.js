87da92f9912f49ad003b3c368cd46ff865e1a32f
Rahul-Tankhiwale <153199890+Rahul-Tankhiwale@users.noreply.github.com>
2025-11-10 10:08:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+let outputDiv = document.getElementById("output");
+function createPromise(name){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(`${name}`)
+		},1000)
+	})
+
+.then((result)=>{
+	outputDiv.textContent = result;
+})
+.catch((err)=>{
+	outputDiv.textContent = "Catch Error"
+})
